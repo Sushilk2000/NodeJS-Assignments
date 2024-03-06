d0f5901470ef1de78c94400a01c1e876aa4ba2e0
SushilKulkarni <sushilkulkarni232@gmail.com>
2024-03-06 15:27:35 +0530

    update

--- Job posting application/backend/routes/jobs.js
+++ Job posting application/backend/routes/jobs.js
@@ -8,4 +8,7 @@ router.post("", jobController.createJob);
 
 router.get("", jobController.getJob);
 
+router.patch("", jobController.editJob);
+
+router.delete("", jobController.deleteJob);
 module.exports = router;
