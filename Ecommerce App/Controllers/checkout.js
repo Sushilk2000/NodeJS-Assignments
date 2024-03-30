4c9bd9af1f5c480b1fbaf032b01411e96b89a5c9
SushilKulkarni <sushilkulkarni232@gmail.com>
2024-03-31 00:40:00 +0530

    testing

--- Ecommerce App/Controllers/checkout.js
+++ Ecommerce App/Controllers/checkout.js
@@ -8,7 +8,7 @@ const { v4: uuidv4 } = require("uuid");
 const { couponModel } = require("../Models/Coupon.js");
 
 dotenv.config();
-
+console.log(process.env.RAZORPAY_KEY_ID);
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
