a45947482261067d31f51505063a951765952fcc
SushilKulkarni <sushilkulkarni232@gmail.com>
2024-04-08 21:25:31 +0530

    fixed

--- server restart/index.js
+++ server restart/index.js
@@ -1,18 +1,32 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const app = express();
+
 async function restartServer() {
-  const restart = await fetch(
-    "https://api.render.com/v1/services/srv-co9i53djm4es73avovcg/restart",
-    {
-      method: "POST",
-      headers: {
-        Authorization: "Bearer rnd_0je2K7yuwyCzQfEwZxnWiwiB14jq",
-      },
+  try {
+    const restart = await fetch(
+      "https://api.render.com/v1/services/srv-co9i53djm4es73avovcg/restart",
+      {
+        method: "POST",
+        headers: {
+          Authorization: "Bearer rnd_0je2K7yuwyCzQfEwZxnWiwiB14jq",
+        },
+      }
+    );
+    if (restart) {
+      console.log("Server restarted at", Date.now());
     }
-  );
-  if (restart) {
-    console.log("Server restarted at", Date.now());
+  } catch (error) {
+    console.error("Error restarting server:", error);
   }
 }
 
 setInterval(() => {
   restartServer();
 }, 600000);
+
+// Use the PORT environment variable provided by Render.com
+const PORT = 10000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
